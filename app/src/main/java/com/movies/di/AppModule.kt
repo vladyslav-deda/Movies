f79e75e119c0d5e5e45e8c124ab05793e8828b3e
Vladyslav Deda <Vladyslav.Deda@cantire.com>
2023-06-15 13:23:23 +0300

    Added modules according to clean architecture

---
+++ app/src/main/java/com/movies/di/AppModule.kt
@@ -0,0 +1,36 @@
+package com.movies.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    private const val BASE_URL = "https://us-central1-testmodule-12b1c.cloudfunctions.net/"
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val okHttpBuilder = OkHttpClient.Builder()
+        okHttpBuilder.addInterceptor(HttpLoggingInterceptor())
+        return okHttpBuilder.build()
+    }
+}
